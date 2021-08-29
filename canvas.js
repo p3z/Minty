55e306aa9b5607e304726879108d7cfbbef7f6bb
p3z2 <chenille.pottle@yahoo.co.uk>
2021-08-29 08:52:28 +0100

    Cause only one to be built at a time

--- canvas.js
+++ canvas.js
@@ -1,16 +1,17 @@
-function initGrid(){
+function build(){
+    // Destroy the previous
+    container.innerHTML = "";
 
-    // Grab the data first (ensure it only updates the array once we've got it)
+    // Grab the data first
     var scannedImg = scanImg(64);
     var pixelsList = [].concat(...scannedImg);
 
-    buildRows(); // Build all rows
-    addPixels(); // Add pixels to them
+    buildRows(); // Build row infrastructure
+    addPixels(); // Add 'pixels' to them
 
     var pixelContainers = document.getElementsByClassName('pixel'); // Grab the newly built 'pixels'
-    // pixelContainers[50].style.background = "red"; // This works as you'd expect
-    
-    // Now paint the rendered div with the data from the image    
+        
+    // Now paint the rendered 'pixel' div with the color data from the image    
     for(var i = 0; i < pixelContainers.length; i++){
         var thisPixel = pixelContainers[i];
         var color = `rgb(${pixelsList[i]})`;
@@ -21,15 +22,10 @@ function initGrid(){
     
 }// end function
 
-window.addEventListener('DOMContentLoaded', () => {
-    
-});
 
-window.onload = function(){
-   
-}
+
 
 btn.addEventListener("click", function(){
-    console.log("Button pressed");
-    initGrid();
+    console.log("Build started");
+    build();
 })

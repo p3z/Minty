c3d73c49bbc27aa98c4f80980508376b2b20b86f
p3z2 <chenille.pottle@yahoo.co.uk>
2021-08-29 11:16:40 +0100

    Proof concept counting the colours

--- canvas.js
+++ canvas.js
@@ -26,10 +26,16 @@ function build(){
             uniqueColors.push(color);
         } // end if
             
+        if(!Object.keys(countCatalog).includes(color)){
+            countCatalog[color] = 1;
+        } else{
+            countCatalog[color]++;
+        }
+
 
     } // End loop
 
-    console.log(uniqueColors)
+    console.log(countCatalog)
    // var swatch = swatchIt(colorsList, "rgb");
    // var paintByNumbers = pixelCounter(colorsList, swatch);
   //  console.log(paintByNumbers);
@@ -42,80 +48,80 @@ function build(){
 //
 /////////////////////////////////////////////////////////
 // Separate out the image data into it's unique colors (we'll mod these later)
-function swatchIt(colorArr, type){
-    // Loop through colour array and return a new array of unique colours
-    var hexArr = [];
-    var rgbArr = [];
+// function swatchIt(colorArr, type){
+//     // Loop through colour array and return a new array of unique colours
+//     var hexArr = [];
+//     var rgbArr = [];
     
-    var uniqueHex = [];
-    var uniqueRgb = [];
-    // Transform all rgb arrays to strings
-    for(var i = 0; i < colorArr.length; i++){
-        var thisColor = colorArr[i].toString();
+//     var uniqueHex = [];
+//     var uniqueRgb = [];
+//     // Transform all rgb arrays to strings
+//     for(var i = 0; i < colorArr.length; i++){
+//         var thisColor = colorArr[i].toString();
 
-        // Translate them to hex for ease of comparison
-        //Break it up
-        var rgb = thisColor.split(",");
+//         // Translate them to hex for ease of comparison
+//         //Break it up
+//         var rgb = thisColor.split(",");
 
-        var r = parseInt(rgb[0]);
-        var g = parseInt(rgb[1]);
-        var b = parseInt(rgb[2]);
+//         var r = parseInt(rgb[0]);
+//         var g = parseInt(rgb[1]);
+//         var b = parseInt(rgb[2]);
 
-        var hexIt = rgbToHex(r, g, b);
-        hexArr.push(hexIt);
+//         var hexIt = rgbToHex(r, g, b);
+//         hexArr.push(hexIt);
         
-        rgbArr.push(thisColor);
-    }
+//         rgbArr.push(thisColor);
+//     }
 
 
-    switch(type){
-        case "hex":
-            hexArr.filter(color => {
-                if(!uniqueHex.includes(color)){
-                    uniqueHex.push(color);
-                } // end if
+//     switch(type){
+//         case "hex":
+//             hexArr.filter(color => {
+//                 if(!uniqueHex.includes(color)){
+//                     uniqueHex.push(color);
+//                 } // end if
                 
-            }); // end filter
+//             }); // end filter
 
-            return uniqueHex;
-            break;
+//             return uniqueHex;
+//             break;
 
-        case "rgb":
-            rgbArr.filter(color => {
-                if(!uniqueRgb.includes(color)){
-                    uniqueRgb.push(color);
-                } // end if
+//         case "rgb":
+//             rgbArr.filter(color => {
+//                 if(!uniqueRgb.includes(color)){
+//                     uniqueRgb.push(color);
+//                 } // end if
                 
-            }); // end filter
-            return uniqueRgb;
-            break;
-    } // end switch
+//             }); // end filter
+//             return uniqueRgb;
+//             break;
+//     } // end switch
     
-} // end function
+// } // end function
 
 // Count the individual instances of each color
 // Return these as an object keyed by the color value
-function pixelCounter(pixels, colors){
-     var countCatalog = {};
+// function pixelCounter(pixels, colors){
+//      var countCatalog = {};
     
-    // // Loop through colors, check the count catalog for an entry if one doesn't exist, create one, otherwise increment it
-    for(var i = 0; i < colors.length; i++){
+//     // // Loop through colors, check the count catalog for an entry if one doesn't exist, create one, otherwise increment it
+//     for(var i = 0; i < colors.length; i++){
 
-        pixels.forEach((pixel) => {
-            // Convert pixel to a string for comparison against colors
-            var thisPixel = pixel.toString(); // String of pixel data (actual pixels)
-            if(colors.includes(thisPixel)){
+//         pixels.forEach((pixel) => {
+//             // Convert pixel to a string for comparison against colors
+//             var thisPixel = pixel.toString(); // String of pixel data (actual pixels)
+//             if(colors.includes(thisPixel)){
 
-            } // 
-        });
+//             } // 
+//         });
 
         
-    } // color loop
+//     } // color loop
         
-    // } // end loop
+//     // } // end loop
 
-    return countCatalog;
-} // end function
+//     return countCatalog;
+// } // end function
 
 
 btn.addEventListener("click", function(){
